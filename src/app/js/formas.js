a38a806d1ade6d02cc1f93bb4a39f70d406d35f3
Gabriel P. Santos <gabrielp_santos@hotmail.com>
2024-05-23 22:44:56 -0300

    :hammer:add: dashboard struct finished

--- src/app/js/formas.js
+++ src/app/js/formas.js
@@ -2,20 +2,79 @@ import formas from '../../../json/formas.json' assert { type: 'json' };
 const pagamentoID = document === null || document === void 0 ? void 0 : document.getElementById("formas-pagamento");
 const envioID = document === null || document === void 0 ? void 0 : document.getElementById("formas-envio");
 const integracoesID = document === null || document === void 0 ? void 0 : document.getElementById("formas-integracoes");
+const condicionalEnvio = (i) => {
+    if ((i % 2 === 1 && i < 4) || i > 7)
+        return 'class="w-28 h-20"';
+    else if (i === 2)
+        return 'class="w-36 h-12 mt-5 mb-1"';
+    else
+        return 'class="size-20"';
+};
+const condicionalIntegracoes = (i) => {
+    if (i === 1)
+        return 'class="size-20 rounded-full overflow-hidden"';
+    else if (i > 2)
+        return 'class="w-32 h-20"';
+    else
+        return 'class="size-20"';
+};
 function pagamentoHTML() {
     const formaPagamento = formas.pagamento;
     pagamentoID.innerHTML = "";
+    //Continue refazendo o código até fica do jeito correto
     for (let i = 0; i < formaPagamento.length; i++) {
-        const elementoPagamento = `
-        <div class="flex flex-col bg-white items-center text-center rounded-md shadow-md">
-            <img src="../../${formaPagamento[i].imagem}" alt=${formaPagamento[i].id}></img>
-            <p class="text-lg mb-3">${formaPagamento[i].nome}</p>
-            <button type="button" class="bg-[#20B9AE] text-white">Selecionar</button>
+        const elementoHTML = `
+        <div class="flex flex-col w-full p-3 bg-white items-center text-center rounded-md shadow-md">
+            <img src="../../${formaPagamento[i].imagem}" alt=${formaPagamento[i].id}
+            class="size-20"></img>
+            <p class="text-lg font-bold text-wrap mt-1.5 mb-3">${formaPagamento[i].nome}</p>
+            <button type="button" 
+            class="bg-[#20B9AE] text-white px-5 py-2 rounded-sm justify-self-center">Selecionar</button>
         </div>
         `;
-        pagamentoID.innerHTML += elementoPagamento;
+        pagamentoID.innerHTML += elementoHTML;
+    }
+}
+function envioHTML() {
+    const formaEnvio = formas.envio;
+    envioID.innerHTML = "";
+    //Continue refazendo o código até fica do jeito correto
+    for (let i = 0; i < formaEnvio.length; i++) {
+        const elementoHTML = `
+        <div class="flex flex-col w-full p-3 bg-white items-center text-center rounded-md shadow-md">
+            <img src="../../${formaEnvio[i].imagem}" alt=${formaEnvio[i].id}
+            ${condicionalEnvio(i)}></img>
+            <p class="text-lg font-bold text-wrap mt-1.5 mb-3">${formaEnvio[i].nome}</p>
+            <button type="button" 
+            class="bg-[#20B9AE] text-white px-5 py-2 rounded-sm justify-self-center">Selecionar</button>
+        </div>
+        `;
+        envioID.innerHTML += elementoHTML;
+    }
+}
+function integracoesHTML() {
+    const formaIntegracoes = formas.integracoes;
+    integracoesID.innerHTML = "";
+    //Continue refazendo o código até fica do jeito correto
+    for (let i = 0; i < formaIntegracoes.length; i++) {
+        const elementoHTML = `
+        <div class="flex flex-col w-full p-3 bg-white items-center text-center rounded-md shadow-md">
+            <img src="../../${formaIntegracoes[i].imagem}" alt=${formaIntegracoes[i].id}
+            ${condicionalIntegracoes(i)}></img>
+            <p class="text-lg font-bold text-wrap mt-1.5 mb-3">${formaIntegracoes[i].nome}</p>
+            <button type="button" 
+            class="bg-[#20B9AE] text-white px-5 py-2 rounded-sm justify-self-center">Selecionar</button>
+        </div>
+        `;
+        integracoesID.innerHTML += elementoHTML;
     }
 }
 if (pagamentoID) {
     pagamentoHTML();
+}
+if (envioID) {
+    envioHTML();
+}
+if (integracoesID) {
+    integracoesHTML();
 }
--- src/app/ts/formas.ts
+++ src/app/ts/formas.ts
@@ -10,6 +10,16 @@ const pagamentoID = <HTMLDivElement>document?.getElementById("formas-pagamento")
 const envioID = <HTMLDivElement>document?.getElementById("formas-envio");
 const integracoesID = <HTMLDivElement>document?.getElementById("formas-integracoes");
 
+const condicionalEnvio =  (i:number):string =>{
+    if((i%2 === 1 && i<4) || i>7) return 'class="w-28 h-20"';
+    else if(i === 2) return 'class="w-36 h-12 mt-5 mb-1"';
+    else return 'class="size-20"';
+}
+const condicionalIntegracoes = (i:number):string =>{
+    if(i === 1) return 'class="size-20 rounded-full overflow-hidden"';
+    else if(i>2) return 'class="w-32 h-20"';
+    else return 'class="size-20"';
+}
 
 function pagamentoHTML(){
     const formaPagamento:Forma[] = formas.pagamento;
@@ -18,19 +28,62 @@ function pagamentoHTML(){
 
     //Continue refazendo o código até fica do jeito correto
     for(let i = 0; i<formaPagamento.length; i++){
-        const elementoPagamento = `
-        <p class="text-xs font-bold mb-2.5"></p>
-        <div class="flex flex-col bg-white items-center text-center rounded-md shadow-md">
-            <img src="../../${formaPagamento[i].imagem}" alt=${formaPagamento[i].id}></img>
-            <p class="text-lg mb-3">${formaPagamento[i].nome}</p>
-            <button type="button" class="bg-[#20B9AE] text-white">Selecionar</button>
+        const elementoHTML = `
+        <div class="flex flex-col w-full p-3 bg-white items-center text-center rounded-md shadow-md">
+            <img src="../../${formaPagamento[i].imagem}" alt=${formaPagamento[i].id}
+            class="size-20"></img>
+            <p class="text-lg font-bold text-wrap mt-1.5 mb-3">${formaPagamento[i].nome}</p>
+            <button type="button" 
+            class="bg-[#20B9AE] text-white px-5 py-2 rounded-sm justify-self-center">Selecionar</button>
         </div>
         `
 
-        pagamentoID.innerHTML += elementoPagamento;
+        pagamentoID.innerHTML += elementoHTML;
     }
 }
 
-if(pagamentoID){
-    pagamentoHTML();
+function envioHTML(){
+    const formaEnvio:Forma[] = formas.envio;
+
+    envioID.innerHTML = "";
+
+    //Continue refazendo o código até fica do jeito correto
+    for(let i = 0; i<formaEnvio.length; i++){
+        const elementoHTML = `
+        <div class="flex flex-col w-full p-3 bg-white items-center text-center rounded-md shadow-md">
+            <img src="../../${formaEnvio[i].imagem}" alt=${formaEnvio[i].id}
+            ${condicionalEnvio(i)}></img>
+            <p class="text-lg font-bold text-wrap mt-1.5 mb-3">${formaEnvio[i].nome}</p>
+            <button type="button" 
+            class="bg-[#20B9AE] text-white px-5 py-2 rounded-sm justify-self-center">Selecionar</button>
+        </div>
+        `
+
+        envioID.innerHTML += elementoHTML;
+    }
 }
+
+function integracoesHTML(){
+    const formaIntegracoes:Forma[] = formas.integracoes;
+
+    integracoesID.innerHTML = "";
+
+    //Continue refazendo o código até fica do jeito correto
+    for(let i = 0; i<formaIntegracoes.length; i++){
+        const elementoHTML= `
+        <div class="flex flex-col w-full p-3 bg-white items-center text-center rounded-md shadow-md">
+            <img src="../../${formaIntegracoes[i].imagem}" alt=${formaIntegracoes[i].id}
+            ${condicionalIntegracoes(i)}></img>
+            <p class="text-lg font-bold text-wrap mt-1.5 mb-3">${formaIntegracoes[i].nome}</p>
+            <button type="button" 
+            class="bg-[#20B9AE] text-white px-5 py-2 rounded-sm justify-self-center">Selecionar</button>
+        </div>
+        `
+
+        integracoesID.innerHTML += elementoHTML;
+    }
+}
+
+if(pagamentoID){pagamentoHTML();}
+if(envioID){envioHTML();}
+if(integracoesID){integracoesHTML();}
